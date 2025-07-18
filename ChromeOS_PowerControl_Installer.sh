4f24be58c964b2cf4bb6b884b2821068ab1262ba
shadowed1 <itszachg@gmail.com>
2025-07-18 00:06:53 -0400

    Update ChromeOS_PowerControl_Installer.sh

--- ChromeOS_PowerControl_Installer.sh
+++ ChromeOS_PowerControl_Installer.sh
@@ -411,7 +411,7 @@ fi
 
 for category in "${ordered_categories[@]}"; do
   echo "# --- ${category} ---" >> "$CONFIG_FILE"
- for key in ${categories[$category]}; do
+  for key in ${categories[$category]}; do
     if [ -n "${!key+x}" ]; then
         if declare -p "$key" 2>/dev/null | grep -q 'declare \-a'; then
             eval "arr=(\"\${${key}[@]}\")"
@@ -428,6 +428,7 @@ for category in "${ordered_categories[@]}"; do
         val="${defaults[$key]}"
         echo "$key=$val" >> "$CONFIG_FILE"
     fi
+  done
 done
 echo "${GREEN}${BOLD}Installing to: $INSTALL_DIR $RESET"
 echo ""
